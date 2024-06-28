b6592871ce4be322b8b1f5e59d0b18745c2c74cc
Boodenator <boode1000@gmail.com>
2024-06-28 06:46:05 -0700

    task1 fx nw

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
-# instal specific version of flask (2.1.0)
-package { 'flask':
+# Installation ofspecific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }

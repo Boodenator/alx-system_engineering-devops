363907aad8c4bd73bc45a06f8d9041c2723deb88
Boodenator <boode1000@gmail.com>
2024-07-02 19:07:59 -0700

    fx

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# puppet-lint ver 2.5.0 installation
-
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+#!/usr/bin/pup
+# Installing specific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
